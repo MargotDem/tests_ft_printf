88ba18261086719574a573d74ac2f86f4b6a42cc
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-01-31 11:38:24 +0200

    tests

---
+++ ints.c
@@ -0,0 +1,50 @@
+int main(void)
+{
+		printf("rl: '%d'\n", -358);
+	ft_printf("ft: '%d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%.d'\n", -358);
+	ft_printf("ft: '%.d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%.4d'\n", -358);
+	ft_printf("ft: '%.4d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%-10.6d'\n", -358);
+	ft_printf("ft: '%-10.6d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%010.6d'\n", -358);
+	ft_printf("ft: '%010.6d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%010d'\n", -358);
+	ft_printf("ft: '%010d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%10d'\n", -358);
+	ft_printf("ft: '%10d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%.6d'\n", -358);
+	ft_printf("ft: '%.6d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%0.6d'\n", -358);
+	ft_printf("ft: '%0.6d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%09.2d'\n", -358);
+	ft_printf("ft: '%09.2d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%09d'\n", -358);
+	ft_printf("ft: '%09d'\n", -358);
+	printf("\n");
+
+	printf("rl: '%9d'\n", -358);
+	ft_printf("ft: '%9d'\n", -358);
+	printf("\n");
+}
